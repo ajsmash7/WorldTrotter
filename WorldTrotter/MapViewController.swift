414045a1170be2d80334d772d8ad17f45eba2145
AJMac <ajmac@ajmacs-imac.local>
2019-02-27 05:58:03 +0000

    Chapter 6 with Dark Mode completed

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,44 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    //override func loadView
+    override func loadView() {
+        //create a mapview
+        mapView = MKMapView()
+        
+        //set as the current view
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: UIControlEvents.valueChanged)
+        
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
+    }
     
     //override viewDidLoad function
     override func viewDidLoad() {
@@ -16,6 +52,19 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         //call print statement to debug view onload
         print("MapViewController loaded its view.")
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = MKMapType.standard
+        case 1:
+            mapView.mapType = MKMapType.hybrid
+        case 2:
+            mapView.mapType = MKMapType.satellite
+        default:
+            break
+        }
     }
     
 }
